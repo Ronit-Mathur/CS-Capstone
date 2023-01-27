3ad872471279f1e8fa1fe44effba365fa6db2129
Owen Russell-Lanning <owenrusselllanning@gmail.com>
2023-01-27 14:31:15 -0800

    Added Client start script for node

---
+++ client.js
@@ -0,0 +1,6 @@
+/**
+ * Root of mental health tracker client project
+ * @author Owen Russell-Lanning, Kyler Greenway, Ronit Mathur
+ */
+
+console.log("[Index] Mental Health Tracker Client is Running");
