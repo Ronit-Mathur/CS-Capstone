f06bef6766e242fa3ca67f3dcf4131c0d3180ec4
Owen Russell-Lanning <owenrusselllanning@gmail.com>
2023-01-27 21:59:46 -0800

    Minor database handler edit

--- backend_server/handlers/databaseHandler.js
+++ backend_server/handlers/databaseHandler.js
@@ -8,15 +8,14 @@ const SERVER_CONSTANTS = require("../serverConstants");
 module.exports = class DatabaseHandler{
 
     constructor(){
-        this.initDatabase(SERVER_CONSTANTS.DATABASE_FILE);
+        this.initDatabase();
     }
 
 
     /**
      * @summary initializes the database. will not overwrite any data. run each time when handler is started
-     * @param path the path of the sqlite database file or where to create one
      */
-    initDatabase(path){
-
+    initDatabase(){
+        
     }
 }
