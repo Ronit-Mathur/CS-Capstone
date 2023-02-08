e3b2832fc8a0c258148670e4b272cd65ae00551e
Owen Russell-Lanning <owenrusselllanning@gmail.com>
2023-02-08 14:32:43 -0800

    updated components

---
+++ components/statscreen.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {Text, TextInput, View, Image, SafeAreaView} from 'react-native';
+
+
+function Stats() {
+    return (
+        <SafeAreaView style ={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text>This is the Stats Page!</Text>
+        </SafeAreaView>
+    );
+  }
+
+  export default Stats;
