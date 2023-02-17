46babfd9f8c872c08afa1b4e253f851a11bc46d5
ronit <ronit@precanto.com>
2023-02-16 23:23:11 -0800

    Tasks Screen (+ Date and Time Picker)

--- components/tasks.tsx
+++ components/tasks.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import {Text, TextInput, SafeAreaView,View} from 'react-native';
-
-
+import {Text, TextInput, SafeAreaView,View, Button} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import RNDateTimePicker from '@react-native-community/datetimepicker';
 
 
 
@@ -11,54 +11,40 @@ function TaskCreation (){
     const [taskTitle, setTaskTitle] = React.useState("");
     const [startTime, setStarttime] = React.useState("");
     const [endTime, setEndTime] = React.useState("");
-    
+    const [showStartTime, setShowStartTime] = React.useState(false);
+    const [showEndTime, setShowEndTime] = React.useState(false);
     return(
         
-        
-        <View >
-            <View style={{
-                paddingLeft:10,
-                paddingRight:10,
-                paddingTop:10,
-                paddingBottom:10
-            }}>
-                <Text>Task Name:</Text>
-                <TextInput 
-                style={{
-                    borderColor:'black', 
-                    borderWidth:1
-                }}
-                onChangeText={newTitle => setTaskTitle(newTitle)}
-                placeholder='Enter Task Name'
-                defaultValue={taskTitle}
-                />   
-                <Text>Start Time:</Text>
-                <TextInput 
-                   style={{
-                    borderColor:'black', 
-                    borderWidth:1, 
-                    
-                }}
-                onChangeText={newStartTime => setStarttime(newStartTime)}
-                placeholder='Enter Task Name'
-                defaultValue={startTime}
-                />
-                   <Text>End Time:</Text>
-                <TextInput 
-                   style={{
-                    borderColor:'black', 
-                    borderWidth:1
-                    
-                }}
-                onChangeText={newEndTime => setEndTime(newEndTime)}
-                placeholder='Enter Task Name'
-                defaultValue={endTime}
-                />
+        <View style={{
+            paddingLeft:10,
+            paddingRight:10,
+            paddingTop:10,
+            paddingBottom:10
+        }}>
+            <TextInput 
+            style={{height: 40}}
+            onChangeText={newTitle => setTaskTitle(newTitle)}
+            placeholder='Enter Task Name'
+            placeholderTextColor={"black"}
+            defaultValue={taskTitle}
+            />
+
+            <View style={{alignItems: 'flex-start',}}>
+
+                <RNDateTimePicker value={new Date()} mode="date" style={{}}/>
                 
+                <Button title='Start Time' onPress={() => (setShowStartTime(!showStartTime))}></Button>
+                {showStartTime ?  <RNDateTimePicker value={new Date()} mode="time" display="spinner"/>: null}
+
+                <RNDateTimePicker value={new Date()} mode="date" style={{}}/>
+
+                <Button title='End Time' onPress={() => (setShowEndTime(!showEndTime))}></Button>
+                {showEndTime ?  <RNDateTimePicker value={new Date()} mode="time" display="spinner"/>: null}
                 
             </View>
-        </View>
 
+        </View>
+        
     );
 }
 
