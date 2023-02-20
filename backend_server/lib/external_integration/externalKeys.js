11a6e572a2630f142f440d77fbeaf9c1fb608dba
Owen Russell-Lanning <owenrusselllanning@gmail.com>
2023-02-20 13:10:44 -0800

    https adjustment

--- backend_server/lib/external_integration/externalKeys.js
+++ backend_server/lib/external_integration/externalKeys.js
@@ -1,7 +1,7 @@
 module.exports = {
     GOOGLE_CLIENT_ID: "858841780627-r0bjoeou764de1r5a2kb90nk926progh.apps.googleusercontent.com",
     GOOGLE_SECRET:"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",
-    GOOGLE_OAUTH_REDIRECT_URL: "http://mentalhealth.ra3.us/api/calendar/import/google/oauth/response",
+    GOOGLE_OAUTH_REDIRECT_URL: "https://mentalhealth.ra3.us/api/calendar/import/google/oauth/response",
 
     AZURE_CLIENT_ID: "84e8fc80-9c14-48d1-8981-f61dda78bbcf",
     AZURE_OAUTH_REDIRECT_URL: "https://mentalhealth.ra3.us/api/calendar/import/outlook/oauth/response",
