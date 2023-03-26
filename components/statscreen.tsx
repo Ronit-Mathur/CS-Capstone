680851e4e209534a52c7536d8ed804107bbdad79
ronit <38735296+Ronit-Mathur@users.noreply.github.com>
2023-03-26 15:38:01 -0700

    Prelim Stats page Heat-map (Hardcoded) & helpers

--- components/statscreen.tsx
+++ components/statscreen.tsx
@@ -6,7 +6,9 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import { useNavigation } from '@react-navigation/native';
 import { Settings } from './settingScreen';
 import calImportPage from './CalImportPage';
-
+import { ContributionGraph } from 'react-native-chart-kit';
+import { Dimensions } from 'react-native';
+import { screenHeight, screenWidth } from 'react-native-calendars/src/expandableCalendar/commons';
 const StackNavigator = createNativeStackNavigator();
 var  user = ''
 
@@ -14,14 +16,45 @@ var  user = ''
 function Stats({Name}:any) {
   user = Name
   const navigation = useNavigation()
+  const commitsData = [
+    { date: "2023-03-02", count: 1 },
+    { date: "2023-03-03", count: 2 },
+    { date: "2023-03-04", count: 3 },
+    { date: "2023-03-05", count: 4 },
+    { date: "2023-03-06", count: 5 },
+    { date: "2023-03-30", count: 2 },
+    { date: "2023-03-31", count: 3 },
+    { date: "2023-04-01", count: 2 },
+    { date: "2023-04-02", count: 4 },
+    { date: "2023-04-05", count: 2 },
+    { date: "2023-04-23", count: 4 }
+  ];
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0,
+    color: (opacity = 0) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
+  const handleToolTip: any = {}
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>This is the Stats Page!</Text>
       {/* <MaterialCommunityIcons name ='calendar-import' color='black' size={55} style={{}} 
         onPress={()=> navigation.navigate('calImport')}
-          /> */}
+      /> */}
+      <ContributionGraph 
+      chartConfig={chartConfig}
+      values={commitsData}
+      tooltipDataAttrs={(value) => handleToolTip}
+      width={screenWidth}
+      height={225}
+      endDate={new Date("2023-08-01")}/>
       <ImportCalendar></ImportCalendar>
-    
+      
     
     </SafeAreaView>
 
