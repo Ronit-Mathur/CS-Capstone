faa3692128ca8a6508d98eb344d71ec51280e8b2
orusselllanning <owenrusselllanning@gmail.com>
2023-04-17 13:30:07 -0700

    switched stats page text to view

--- components/newStatsScreen.js
+++ components/newStatsScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Button } from "react-native"
+import { View, Text, Button, SafeAreaView } from "react-native"
 import { Component } from "react"
 import StylingConstants from "./StylingConstants";
 import { ScrollView } from "react-native-gesture-handler";
@@ -9,6 +9,7 @@ import serverHandler from '../lib/server/serverHandler';
 import DaysOfWeekStatsWidget from './widgets/daysOfWeekStatsWidget'
 import TaskWidget, { HappiestWhenDayStartsWithWidget, LeastEnjoyableTaskWidget } from "./widgets/taskWidget";
 
+
 export default class StatsScreen extends Component {
 
     constructor(props) {
@@ -17,28 +18,34 @@ export default class StatsScreen extends Component {
 
     render() {
         return (
-            <View style={{ flex: 1, flexDirection: "column" }}>
-                <Text style={{
-                    color: "white",
+            <SafeAreaView style={{ flex: 1, flexDirection: "column" }}>
+                <View style={{
                     borderBottomLeftRadius: 12,
-                    borderBottomRightRadius: 12, backgroundColor: StylingConstants.highlightColor, padding: "4%", fontFamily: StylingConstants.defaultFontBold, fontSize: StylingConstants.hugeFontSize
-                }}>Your Statistics</Text>
-
+                    borderBottomRightRadius: 12, backgroundColor: StylingConstants.highlightColor, padding: "4%",
+                }}>
+                    <Text style={{
+                        color: "white",
+                        fontFamily: StylingConstants.defaultFontBold, fontSize: StylingConstants.hugeFontSize
+                    }}>Your Statistics</Text>
+                </View>
                 <ScrollView style={{ padding: "2%", height: "100%" }}>
 
                     <TotalsWidget></TotalsWidget>
                     <FiveDayMoodHistoryWidget></FiveDayMoodHistoryWidget>
                     <LeastEnjoyableTaskWidget></LeastEnjoyableTaskWidget>
                     <HappiestWhenDayStartsWithWidget></HappiestWhenDayStartsWithWidget>
+                    {/** 
                     <View style={{ marginTop: "4%" }}>
 
                         <DaysOfWeekStatsWidget />
                         <Button title='calcDayMood' onPress={() => calcDayMood(serverHandler.current.userState.username, '04/11/2023')}></Button>
                         <Button title='monthRatedTasks' onPress={() => getTaskRatingsMonth('04/2023')}></Button>
                     </View>
+            **/}
                 </ScrollView>
 
-            </View>
+
+            </SafeAreaView>
         )
     }
 }
